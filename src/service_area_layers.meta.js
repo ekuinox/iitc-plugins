52d58eda22221adc5d590c59ce11a0a63b5014c0
lm9 <depkey98@gmail.com>
2017-07-06 11:32:10 +0900

    Update service_area_layers.meta.js

--- src/service_area_layers.meta.js
+++ src/service_area_layers.meta.js
@@ -2,7 +2,7 @@
 // @id             iitc-plugin-service-area-maps
 // @name           IITC plugin: Service Area Maps
 // @category       Layer
-// @version        0.0.1.20170706.040000
+// @version        0.0.1.20170706.050000
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://raw.githubusercontent.com/lm9/iitc-plugins/master/src/service_area_layers.meta.js
 // @downloadURL    https://raw.githubusercontent.com/lm9/iitc-plugins/master/src/service_area_layers.user.js
