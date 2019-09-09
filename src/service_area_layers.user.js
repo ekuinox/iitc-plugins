1383a64cc32f5ad19745eabd8ddaeeb7d689cc89
ekuinox <depkey@me.com>
2019-09-09 19:33:54 +0900

    バージョン更新

--- src/service_area_layers.user.js
+++ src/service_area_layers.user.js
@@ -2,11 +2,11 @@
 // @id             iitc-plugin-service-area-maps
 // @name           IITC plugin: Service Area Maps
 // @category       Layer
-// @version        0.0.3.20171011.010000
+// @version        0.0.4.20190909.010000
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://raw.githubusercontent.com/ekuinox/iitc-plugins/master/src/service_area_layers.meta.js
 // @downloadURL    https://raw.githubusercontent.com/ekuinox/iitc-plugins/master/src/service_area_layers.user.js
-// @description    [bvq-2017-07-06-040000] au, DoCoMo, SoftBankのサービスエリアマップをレイヤへ追加します．
+// @description    [bvq-2019-09-09-010000] au, DoCoMo, SoftBankのサービスエリアマップをレイヤへ追加します．
 // @include        https://*.ingress.com/intel*
 // @include        http://*.ingress.com/intel*
 // @match          https://*.ingress.com/intel*
